be0281ac4283f9bb913d5d55078791c9b17a22e9
bodyan <bodyanua@gmail.com>
2016-10-08 21:25:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require_once 'class/SMFLocaleClass.php';
 $file = new SMFLocale();
 
 //folders
-$english    = 'english';
-$input =  'translation';
-$output = 'output';
+$english = 'english';
+$input   = 'translation';
+$output  = 'output';
 
 $file->completeTranslation($english , $input, $output)
 
